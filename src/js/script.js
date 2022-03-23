5aaf0725cfbc61bed90fb1b2f796befdb2b940e8
Arximed-A <77243691+Arximed-A@users.noreply.github.com>
2022-03-23 08:57:59 +0300

    Add files via upload
    Пример работы по html и css

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+/*
+let login = prompt('Кто там?', '');
+console.log(login);
+console.log(typeof login);
+if (login === 'Админ') {
+}
+else if (String(login)) {
+	alert('Я вас не знаю')
+}
+else if (login == '') {
+	alert('Что ок?')
+}
+else if (login === null) {
+	alert('Отмена')
+}
+else {
+	alert('От')
+}
+*/
+
+function foo() {
+	foo();
+}
+
+foo();
